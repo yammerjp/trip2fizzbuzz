35281d47cfcda156e09a2ddb6aef1a2bd977799b
Keisuke Nakayama <me@yammer.jp>
2024-12-27 02:31:54 +0900

    fizzbuzz

--- lib/trip2fizzbuzz.rb
+++ lib/trip2fizzbuzz.rb
@@ -4,5 +4,19 @@ require_relative "trip2fizzbuzz/version"
 
 module Trip2fizzbuzz
   class Error < StandardError; end
-  # Your code goes here...
+  class CLI
+    def self.start
+      (1..ARGV[0].to_i).each do |i|
+        if i%15 == 0
+          puts "fizzbuzz"
+        elsif i%3 == 0
+          puts "fizz"
+        elsif i%5 == 0
+          puts "buzz"
+        else
+          puts i
+        end
+      end
+    end
+  end
 end
